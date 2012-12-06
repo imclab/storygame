d65b2f5011fe71cd978b476a7dce911d97fd5963
Matias Saguir <mativs@gmail.com>
2012-12-06 15:36:31 -0200

    Add is_active on admin membership

--- storygame/stories/admin.py
+++ storygame/stories/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 class StoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+class MembershipAdmin(admin.ModelAdmin):
+	list_display = ('__unicode__', 'is_active',)
+
 admin.site.register(Story, StoryAdmin)
 admin.site.register(Line)
-admin.site.register(Membership)
+admin.site.register(Membership, MembershipAdmin)
